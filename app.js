154e89de33f4a2ee843bada51047c88f1ae27bad
unknown <palak.singh.19cse@bmu.edu.com>
2021-06-30 11:46:18 +0530

    Change to package.json

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.statis('client/build'))
+    app.use(express.static('client/build'))
     const path = require('path')
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(_dirname,'client','build','index.html'))
